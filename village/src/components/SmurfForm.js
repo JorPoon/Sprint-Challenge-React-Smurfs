3cde4e242affd0d738d6cfe6613cbd8de6db753f
Jor_Poon <jormpoon@gmail.com>
2019-03-01 12:33:45 -0500

    new stuff

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -16,18 +16,18 @@ class SmurfForm extends Component {
     event.preventDefault();
     // add code to create the smurf using the api
     axios
-    .post('http://localhost:3333/smurfs')
+    .post('http://localhost:3333/smurfs', {name: this.state.name, age: this.state.age, height: this.state.height})
     .then(res => {
       console.log(res)
-      this.setState({
-        name: res.name,
-        age: res.age,
-        height: res.height
-      });
     })
     .catch(err => {
       console.log(err);
     })
+    this.setState({
+      name: '',
+      age: '',
+      height: ''
+    });
   }
 
   handleInputChange = e => {
